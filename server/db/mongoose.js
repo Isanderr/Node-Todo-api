05bb3bea4e5f5eaf5233f07fb524b555a9e036e7
Isanderr <isander@wp.pl>
2018-11-10 19:31:09 +0100

    Add delete /todos/:id route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -12,36 +12,36 @@
 
 
 
-var mongoose = require('mongoose');
-
-mongoose.Promise = global.Promise;
-
-mongoose.connect(process.env.MONGODB_URI, {
-    useMongoClient: true
-}).then(
-    () => {}
-);
-
-
 // var mongoose = require('mongoose');
 
 // mongoose.Promise = global.Promise;
 
-// let db = {
-//     localhost: 'mongodb://localhost:27017/TodoApp',
-//     mlab: 'mongodb://test:test123@ds259253.mlab.com:59253/node_todo_app'
-// };
-
-// mongoose.connect(db.localhost, {
+// mongoose.connect(process.env.MONGODB_URI, {
 //     useMongoClient: true
 // }).then(
-//     () => {},
-//     err => {
-//         mongoose.connect(db.mlab, {
-//             useMongoClient: true
-//         });
-//     }
+//     () => {}
 // );
 
 
-// module.exports = { mongoose };
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+let db = {
+    localhost: 'mongodb://localhost:27017/TodoApp',
+    mlab: 'mongodb://test:test123@ds259253.mlab.com:59253/node_todo_app'
+};
+
+mongoose.connect(db.localhost, {
+    useMongoClient: true
+}).then(
+    () => {},
+    err => {
+        mongoose.connect(db.mlab, {
+            useMongoClient: true
+        });
+    }
+);
+
+
+module.exports = { mongoose };
