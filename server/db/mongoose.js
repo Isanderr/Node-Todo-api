a5ca4f944990e85f2e9ff1da90b14fd0091208f1
Isanderr <isander@wp.pl>
2018-11-10 17:31:17 +0100

    add mlab and heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,11 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+// mongoose.connect('mongodb://localhost:27017/TodoApp');
+let db = {
+    localhost: 'mongodb://localhost:27017/TodoApp',
+    mlab: 'mongodb://test:test123@ds259253.mlab.com:59253/node_todo_app'
+};
 
+mongoose.connect(db.mlab || db.localhost);
 module.exports = { mongoose };
