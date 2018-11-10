006c4093f88c2fea32ca381722d967d1dfb382f9
Isanderr <isander@wp.pl>
2018-11-10 18:30:08 +0100

    change in mlab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -10,25 +10,38 @@
 // // mongoose.connect(db.mlab || db.localhost);
 // module.exports = { mongoose };
 
+
+
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-let db = {
-    localhost: 'mongodb://localhost:27017/TodoApp',
-    mlab: 'mongodb://test:test123@ds259253.mlab.com:59253/node_todo_app'
-};
-
-mongoose.connect(db.localhost, {
+mongoose.connect(process.env.MONGODB_URI, {
     useMongoClient: true
 }).then(
-    () => {},
-    err => {
-        mongoose.connect(db.mlab, {
-            useMongoClient: true
-        });
-    }
+    () => {}
 );
 
 
-module.exports = { mongoose };
+// var mongoose = require('mongoose');
+
+// mongoose.Promise = global.Promise;
+
+// let db = {
+//     localhost: 'mongodb://localhost:27017/TodoApp',
+//     mlab: 'mongodb://test:test123@ds259253.mlab.com:59253/node_todo_app'
+// };
+
+// mongoose.connect(db.localhost, {
+//     useMongoClient: true
+// }).then(
+//     () => {},
+//     err => {
+//         mongoose.connect(db.mlab, {
+//             useMongoClient: true
+//         });
+//     }
+// );
+
+
+// module.exports = { mongoose };
