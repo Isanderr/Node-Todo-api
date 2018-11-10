d69086a069d2660264cea27dc5920b30874595a8
Isanderr <isander@wp.pl>
2018-11-10 18:02:38 +0100

    add new version of mlabv2

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,11 +1,21 @@
+// var mongoose = require('mongoose');
+
+// mongoose.Promise = global.Promise;
+// mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/TodoApp');
+// // let db = {
+// //     localhost: 'mongodb://localhost:27017/TodoApp',
+// //     mlab: 'mongodb://test:test123@ds259253.mlab.com:59253/node_todo_app'
+// // };
+
+// // mongoose.connect(db.mlab || db.localhost);
+// module.exports = { mongoose };
+
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/TodoApp');
-// let db = {
-//     localhost: 'mongodb://localhost:27017/TodoApp',
-//     mlab: 'mongodb://test:test123@ds259253.mlab.com:59253/node_todo_app'
-// };
 
-// mongoose.connect(db.mlab || db.localhost);
-module.exports = { mongoose };
+mongoose.connect(process.env.MONGODB_URI, {
+    useMongoClient: true
+}).then(
+    () => {}
+);
