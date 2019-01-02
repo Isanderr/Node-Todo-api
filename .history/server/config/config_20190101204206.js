21a27cd32d51229eecf24845a1ed6cd85bc8d995
Isanderr <isander@wp.pl>
2019-01-02 21:46:44 +0100

    working local problems with heroku

---
+++ .history/server/config/config_20190101204206.js
@@ -0,0 +1,19 @@
+var env = process.env.NODE_ENV || 'development';
+
+if (env === 'development' || env === 'test')
+{
+    var config = require('./config.json');
+    var envConfig = config[ env ];
+
+    console.log(Object.keys(envConfig));
+}
+
+// if (env === 'development')
+// {
+//     process.env.PORT = 3000;
+//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+// } else if (env === 'test')
+// {
+//     process.env.PORT = 3000;
+//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+// }
